180e82c0c4908e40c9d648825c89882c5a5de614
tbaddade <thomas@redaxo.org>
2020-02-12 21:15:41 +0100

    PlugIn linkYForm :: es können jetzt Links zu den Datensätzen von YForm eingefügt werden

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Redactor Editor
 
-#### Plugins
+## Plugins
 
 - blockquote
 - bold
@@ -17,7 +17,8 @@
 - italic
 - linkExternal
 - linkInternal
-- lists[ul,ol,indent,outdent]
+- linkYForm[tableNameA=field|rex_yf_news=title]
+- lists[ul|ol|indent|outdent]
 - ol
 - outdent
 - pre 
@@ -30,7 +31,7 @@
 - undo
 
 
-#### Redactor Plugins
+## Redactor Plugins
 
 - counter (via Profil aktivieren)
 - limiter (via Profil aktivieren)
@@ -38,3 +39,45 @@
 - table
 - video
 - widget
+
+
+## Hinweise 
+
+### Plugin linkYForm
+
+Um die fiktiven generierten Urls wie `rex-yf-news://1` zu ersetzen, muss folgendes Skript in die `boot.php` des `project` AddOns.
+Dazu müsste der Code für die Urls angepasst werden. 
+
+```php
+\rex_extension::register('OUTPUT_FILTER', function(\rex_extension_point $ep) {
+    return preg_replace_callback(
+        '@(rex-yf-(news|person))://(\d+)(?:-(\d+))?/?@i',
+        function ($matches) {
+            // table = $matches[1]
+            // id = $matches[3]
+            $url = '';
+            switch ($matches[1]) {
+                case 'rex-yf-news':
+                    // Beispiel, falls die Urls via Url-AddOn generiert werden 
+                    $object = News::get($matches[3]);
+                    if ($object) {
+                        $url = $object->getUrl();
+                        
+                        // die getUrl Methode könnte so aussehen
+                        // public function getUrl()
+                        // {
+                        //     return rex_getUrl('', '', ['news-id' => $this->id]);
+                        // }
+                    }
+                    break;
+                case 'rex-yf-person':
+                    // ein anderes Beispiel 
+                    $url = /index.php?person=$matches[3];
+                    break;
+            }
+            return $url;
+        },
+        $ep->getSubject()
+    );
+}, rex_extension::NORMAL);
+```
--- assets/plugins/linkYForm.js
+++ assets/plugins/linkYForm.js
@@ -22,31 +22,50 @@
             this.toolbar = app.toolbar;
             this.insertion = app.insertion;
             this.selection = app.selection;
+
+            if ('redaxo' in this.opts && 'linkYForm' in this.opts.redaxo) {
+                this.yform = app.opts.redaxo.linkYForm;
+            } else {
+                return false;
+            }
         },
 
         // public
         start: function () {
+            let dropdown = {};
+
+            $.each(this.yform, function(i, data) {
+                dropdown[i] = {
+                    title: data[0],
+                    api: 'plugin.linkYForm.open',
+                    args: {
+                        table: data[0],
+                        label: data[1]
+                    }
+                };
+            });
+
             let obj = {
-                title: redactorTranslations.linkInternal_title,
-                observe: 'link',
+                title: redactorTranslations.linkYForm_title,
                 api: 'plugin.linkYForm.open'
             };
 
             let button = this.toolbar.addButton('linkYForm', obj);
-            button.setIcon('<i class="rex-icon rex-icon-open-linkmap"></i>');
+            button.setDropdown(dropdown);
         },
 
-        open: function () {
+        open: function (data) {
             let that = this;
             let eventFired = false;
-            let pool = newPoolWindow('index.php?page=yform/manager/data_edit&table_name=rex_yform_test&rex_yform_manager_opener[id]=1&rex_yform_manager_opener[field]=last_name&rex_yform_manager_opener[multiple]=0');
+            let pool = newPoolWindow('index.php?page=yform/manager/data_edit&table_name=' + data.table + '&rex_yform_manager_opener[id]=1&rex_yform_manager_opener[field]=' + data.label + '&rex_yform_manager_opener[multiple]=0');
             $(pool).on('rex:YForm_selectData', function (event, id, label) {
                 event.preventDefault();
                 pool.close();
 
                 if (!eventFired) {
+                    label = label.replace(new RegExp(that.opts.redaxo.regex.id, 'gi'), "$1");
                     let options = {
-                        url: 'rex-yform-test://' + id,
+                        url: data.table.split('_').join('-') + '://' + id,
                         label: label
                     };
                     that._insert(options);
