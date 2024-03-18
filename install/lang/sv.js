f1b6666295215f19fc7aaead48b6a8da82c5b91d
Alexander Walther <mail@alexplus.de>
2024-03-18 12:07:21 +0100

    Editor-Übersetzungen (de, en, es, fr, sv)

---
+++ install/lang/sv.js
@@ -0,0 +1,108 @@
+Redactor.lang['sv'] = {
+    "accessibility": {
+        "help-label": "Éditeur de texte riche"
+    },
+    "placeholders": {
+        "figcaption": "Entrez une légende d'image (facultatif)"
+    },
+    "modal": {
+        "link": "Lien",
+        "image": "Image",
+        "add-image": "Ajouter une image"
+    },
+    "embed": {
+        "embed": "Intégrer",
+        "caption": "Légende",
+        "description": "Insérez un code d'intégration/HTML ou entrez l'URL (vidéo Vimeo ou YouTube uniquement)",
+        "responsive-video": "Vidéo responsive"
+    },
+    "image": {
+        "or": "ou",
+        "alt-text": "Texte alternatif",
+        "link": "Lien",
+        "width": "Largeur",
+        "caption": "Légende",
+        "link-in-new-tab": "Ouvrir le lien dans un nouvel onglet",
+        "url-placeholder": "Insérez l'URL de l'image...",
+        "upload-new-placeholder": "Glissez une nouvelle image ici pour la télécharger<br>ou cliquez pour sélectionner"
+    },
+    "link": {
+        "link": "Lien",
+        "edit-link": "Modifier le lien",
+        "unlink": "Supprimer le lien",
+        "link-in-new-tab": "Ouvrir le lien dans un nouvel onglet",
+        "text": "Texte",
+        "url": "URL"
+    },
+    "layout": {
+        "single-column": "Enkel kolumn",
+        "two-columns": "Två kolumner",
+        "three-columns": "Tre kolumner",
+        "four-columns": "Fyra kolumner"
+    },
+    "outset": {
+        "outset-none": "Ingen förskjutning",
+        "outset-left": "Förskjutning vänster",
+        "outset-both": "Förskjutning både",
+        "outset-right": "Förskjutning höger"
+    },
+    "wrap": {
+        "wrap-none": "Ingen omslag",
+        "wrap-left": "Omslag vänster",
+        "wrap-center": "Omslag center",
+        "wrap-right": "Omslag höger"
+    },
+    "blocks": {
+        "address": "Adress",
+        "cell": "Cell",
+        "column": "Kolumn",
+        "dlist": "Definitionslista",
+        "embed": "Bädda in",
+        "figcaption": "Bildtext",
+        "heading": "Rubrik",
+        "image": "Bild",
+        "wrapper": "Omslag",
+        "layout": "Layout",
+        "line": "Linje",
+        "list": "Lista",
+        "listitem": "Objekt",
+        "noneditable": "Icke-redigerbar",
+        "pre": "För",
+        "quote": "Citat",
+        "row": "Rad",
+        "table": "Tabell",
+        "text": "Text",
+        "todo": "Att göra",
+        "todoitem": "Objekt",
+        "mergetag": "Sammanfoga tagg"
+    },
+    "hotkeys": {
+        "meta-shift-a": "Välj text i blocket",
+        "meta-a": "Välj alla block",
+        "meta-z": "Ångra",
+        "meta-shift-z": "Gör om",
+        "meta-shift-m": "Ta bort inline format",
+        "meta-b": "Fet",
+        "meta-i": "Kursiv",
+        "meta-u": "Understrykning",
+        "meta-h": "Upphöjd text",
+        "meta-l": "Nedsänkt text",
+        "meta-k": "Länk",
+        "meta-alt-0": "Normal text",
+        "meta-alt-1": "Rubrik 1",
+        "meta-alt-2": "Rubrik 2",
+        "meta-alt-3": "Rubrik 3",
+        "meta-alt-4": "Rubrik 4",
+        "meta-alt-5": "Rubrik 5",
+        "meta-alt-6": "Rubrik 6",
+        "meta-shift-7": "Ordnad lista",
+        "meta-shift-8": "Oordnad lista",
+        "meta-indent": "Indrag",
+        "meta-outdent": "Minska indrag",
+        "meta-shift-backspace": "Ta bort block",
+        "meta-shift-o": "Lägg till block",
+        "meta-shift-d": "Duplicera block",
+        "meta-shift-up": "Flytta rad upp",
+        "meta-shift-down": "Flytta rad ner"
+    }
+};
